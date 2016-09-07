833feb450b14982b2ed44f9e0edebe09b68f1994
Elizabeth Jones <ElizabethJones@Elizabeths-MacBook-Pro-3.local>
2016-09-07 11:23:57 +1000

    memory game unit 9 update

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,5 +3,11 @@ var cardTwo =  "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log(cardOne == cardTwo); //check comparison is true
-console.log(cardOne == cardTwo); //check comparison is false
+//console.log(cardOne == cardTwo); //check comparison is true
+//console.log(cardOne == cardThree); //check comparison is false
+
+if(cardOne === cardTwo || cardThree === cardFour){
+	alert("You found a match!");
+}else {
+	alert("Sorry, try again.")
+}
